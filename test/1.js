90d78fa05474531b750f6476092d7865736ef329
Agent47 <654136828@qq.com>
2020-01-31 11:27:13 +0800

    test3

--- test/1.js
+++ test/1.js
@@ -1,8 +1,234 @@
-1
-2
-3
-4
-5
-6
-6
-7
+const txt = `
+Targets
+    Occurrences of '东方赢家财富版' in Project (Except Comments)
+Found Occurrences  (96 usages found)
+    Unclassified occurrence  (79 usages found)
+        src\\account-myaccount2\\views  (1 usage found)
+            myAccount.js  (1 usage found)
+                167 <span class="red">提示：</span>如需变更手机号、银行卡、或删除手机号、银行卡、资金账户，可下载东方赢家财富版APP进行操作。
+        src\\account-myaccount\\views  (1 usage found)
+            myAccount.js  (1 usage found)
+                167 <span class="red">提示：</span>如需变更手机号、银行卡、或删除手机号、银行卡、资金账户，可下载东方赢家财富版APP进行操作。
+        src\\activity-hot\\views  (8 usages found)
+            financialInfo.js  (2 usages found)
+                46 1、您可以登录东方赢家财富版APP，进入理财首页进行新客理财；<br/>
+                47 2、完成活动要求后，金元宝将在3个交易日内到账，您可以进入东方赢家财富版APP首页—我的—我的金元宝中查询；
+            goldInfo.js  (1 usage found)
+                44 3、领取的奖励可以在东方赢家财富版APP首页—我的—我的金元宝中查询；
+            noviceInfo.js  (2 usages found)
+                46 1、您可以登录东方赢家财富版APP，进入理财首页进行现金宝充值；<br/>
+                47 2、充值成功之后，金元宝将在3个交易日内到账，您可以进入东方赢家财富版APP首页—我的—我的金元宝中查询；
+            rule.js  (3 usages found)
+                45 1、用户可以登录东方赢家财富版APP—热门活动进行签到、捡金元宝；<br/>
+                46 2、签到需要绑定手机号，用户可进入东方赢家财富版APP首页—我的-我的信息-手机号进行手机号绑定；<br/>
+                47 3、用户可以登录东方赢家财富版APP—热门活动-查看金元宝数目，点击金元宝数目可以查看金元宝记录；
+        src\\activity-share\\views  (6 usages found)
+            shareIndex.js  (3 usages found)
+                29 <div class="ui-col">即日起，凡通过东方赢家财富版APP专属活动页面成功邀请好友投资“新手专享”特定产品即可参与双重奖励，奖励无上限。邀请越多，拿奖越高! </div>
+                41 <p>1、用户在东方赢家财富版APP或微信中完成实名注册，即可分享专属页面推荐好友实名认证或购买；</p>
+                45 <p>新手理财购买路径：东方赢家财富版APP > 理财 > 新手专享</p>
+            shareRecommend.js  (3 usages found)
+                28 <div class="ui-col">即日起，凡通过东方赢家财富版APP专属活动页面成功邀请好友投资“新手专享”特定产品即可参与双重奖励，奖励无上限。邀请越多，拿奖越高! </div>
+                42 <p>1、用户在东方赢家财富版APP或微信中完成实名注册，即可分享专属页面推荐好友实名认证或购买；</p>
+                46 <p>新手理财购买路径：东方赢家财富版APP > 理财 > 新手专享</p>
+        src\\business-orientcash\\views  (1 usage found)
+            rule.js  (1 usage found)
+                209 东方证券和基金管理人可以根据业务情况对快速转出规则进行变更，变更时将通过东方赢家财富版APP等平台予以公告。购买货
+        src\\business-orienthui\\views  (1 usage found)
+            transferOutAgreement.js  (1 usage found)
+                112 东方证券和基金管理人可以根据业务情况对快速转出规则进行变更，变更时将通过东方赢家财富版APP等平台予以公告。购买货
+        src\\business-pwdreset\\views  (1 usage found)
+            result.js  (1 usage found)
+                53 交！如有问题请通过东方赢家财富版APP进入在线
+        src\\common\\api\\model  (1 usage found)
+            readme.md  (1 usage found)
+                14 在东方赢家财富版APP内H5页面统一由refreshToken指令和桥接来完成H5和客户端之间的授信，因此从设计上这些授信后的数据并不会被持久化，而是在内存中存储。而在非APP的容器内则是通过账户登录来完成授权，通过登录(包括登录密码/短信验证码/资金账户登录)后授权数据会被归档，在每一次需要使用到数据时再进行接档和数据恢复。  
+        src\\financing-cashcoinredeem\\views  (1 usage found)
+            rule.js  (1 usage found)
+                59 东方赢家财富版APP等平台予以公告
+        src\\financing-cashcoinsubscribe\\views  (1 usage found)
+            rule.js  (1 usage found)
+                71 东方赢家财富版APP等平台予以公告
+        src\\financing-introduction\\views  (1 usage found)
+            main.js  (1 usage found)
+                40 <p>金元宝是东方证券会员积分，是东方证券为回报广大用户所提供的增值服务之一。用户可按照“100金元宝价值1元”比例兑换话费等多种礼品。登录【东方赢家财富版APP】-【热门活动】即可查看“我的金元宝”。</p>
+        src\\financing-myfund\\views  (5 usages found)
+            faq.js  (5 usages found)
+                287 <p>目前东方赢家财富版APP支持按月、按周、按日定投。</p>
+                294 20、如何在东方赢家财富版APP办理基金交易撤单？
+                300 <p>撤单操作路径为，登录“东方赢家财富版APP-理财-我的-交易记录”，选中需要撤单的交易，点击“撤单”按钮即可撤单。</p>
+                307 21、东方赢家财富版APP如何变更分红方式？
+                311 <p>您可以在基金分红权益登记日前一个交易日15点前，登录东方赢家财富版APP“理财-我的-基金-分红方式”或“交易-开放式基金-分红设置”变更。您可重复变更选择的分红方式，但最终分红状态以基金分红权益登记日之前最后一次的成功申请为准。货币型基金分红方式为红利再投资，不可变更。</p>
+        src\\financing-newlogin\\views  (1 usage found)
+            main.js  (1 usage found)
+                24 <li>1、即日起，用户使用资金账号首次登录东方赢家财富版APP即可领取200金元宝。</li>
+        src\\fund-data\\views  (1 usage found)
+            main.js  (1 usage found)
+                42 <p>数据说明：数据来源wind、东方赢家财富版APP搜索数据等，更新时间 T-2。</p>
+        src\\fund-hot\\views  (1 usage found)
+            home.js  (1 usage found)
+                25 <p class="risk-tip">数据说明：数据来源wind、东方赢家财富版APP搜索数据等，更新时间 T-2。</p>
+        src\\home-informationdetail  (6 usages found)
+            article.ftl  (4 usages found)
+                23     <span style="display:{display}">打开东方赢家财富版查看</span>
+                63       <img src="\${jsDomain}/html5-openlibs/news/v1.0.0/mask.jpg" alt="打开东方赢家财富版查看全文" title="打开东方赢家财富版查看全文"/>
+                63       <img src="\${jsDomain}/html5-openlibs/news/v1.0.0/mask.jpg" alt="打开东方赢家财富版查看全文" title="打开东方赢家财富版查看全文"/>
+                64       <button class="open-dfyj">打开东方赢家财富版查看全文</button>
+            index.html  (2 usages found)
+                30 <span style="display:{display}">打开东方赢家财富版查看</span>
+                70 <button class="open-dfyj">打开东方赢家财富版查看全文</button>
+        src\\introduce-activity\\views  (1 usage found)
+            main.js  (1 usage found)
+                30 <div class="rule-text"><p>参与奖：</p>通过本活动页面，每邀请1位好友完成手机号码登记，并使用该手机号码在从未完成手机号码注册过的东方赢家财富版APP的设备（手机）上注册并登陆，即可获得200金元宝奖励。
+        src\\introduce-share  (1 usage found)
+            index.html  (1 usage found)
+                7 <title>东方赢家财富版</title>
+        src\\introduce-share\\views  (1 usage found)
+            template.js  (1 usage found)
+                37 <h1>东方赢家财富版</h1>
+        src\\level2-activity  (1 usage found)
+            index.html  (1 usage found)
+                7 <title>东方赢家财富版Level2</title>
+        src\\level2-activity\\views  (3 usages found)
+            main.js  (3 usages found)
+                34 <h1>东方赢家财富版Level2</h1>
+                47 <p>2.所有东方赢家财富版APP手机注册用户均可支付0.01元参加Level-2的极速行情体验活动。
+                49 <p>3.Level2权限仅限在东方赢家财富版APP移动端APP3.8.0或以上版本使用。</p>
+        src\\marketing-appdownload\\views  (1 usage found)
+            appdownload.js  (1 usage found)
+                41 <h3>东方赢家财富版</h3>
+        src\\marketing-cmbc\\views  (1 usage found)
+            info.js  (1 usage found)
+                21 <p class="data-info">数据说明：数据来源wind,播报日期{model.dataDate}.过往完整业绩请登录东方赢家财富版 APP查看产品页详情。</p>
+        src\\marketing-crjh3  (4 usages found)
+            crjh-rules.html  (4 usages found)
+                48 <p>下载安装东方赢家财富版APP</p>
+                187 <p>活动期间，客户完成活动要求，即可进入东方赢家财富版APP，点击【行情/自选】-【个股】，或点击【我的】-【我的Level2】即可获得的免费Level-2。</p>
+                189 <p>进入东方赢家财富版APP理财频道，点击【新手专享】或【基金】根据产品要求购买产品。</p>
+                195 <p>7、东方证券可以根据本活动的实际开展情况，对活动规则进行变动或调整，相关变动或调整将在东方赢家财富版APP或活动页面上公布，东方证券有权在法律法规的规定范围内对活动规则进行解释，如在活动过程中出现涉嫌违规与欺诈的行为，东方证券具有追责的权利。</p>
+        src\\marketing-crjh3\\views  (1 usage found)
+            success.js  (1 usage found)
+                26 <p>下载东方赢家财富版APP</p>
+        src\\marketing-fundFT\\views  (2 usages found)
+            home.js  (2 usages found)
+                35 3、长跑杯活动对象：活动期间，通过东方赢家财富版APP连续成功定投的新贵杯获奖者可分享奖学金。成功定投的定义：活动期间，90天内成功定投（同一产品）扣款次数不小于3次。<br/>
+                37 5、以上奖励以等价值金元宝形式发放。金元宝是东方证券会员积分，可按照“100金元宝价值1元”比例兑换话费等多种礼品。登录【东方赢家财富版APP】-【我的】-【我的金元宝】即可领取。<br/>
+        src\\marketing-install  (1 usage found)
+            index.html  (1 usage found)
+                7 <title>东方赢家财富版</title>
+        src\\marketing-install\\views  (1 usage found)
+            template.js  (1 usage found)
+                57 <h1>东方赢家财富版</h1>
+        src\\marketing-lc818\\views  (2 usages found)
+            main.js  (1 usage found)
+                193 奖励。客户可在东方赢家财富版APP【热门活动】中，按照100：1的价值比例兑换等值话费等礼品。温馨提示：金元宝需要实名认证后才能兑换。
+            mainanswer.js  (1 usage found)
+                111 奖励。客户可在东方赢家财富版APP【热门活动】中，按照100：1的价值比例兑换等值话费等礼品。温馨提示：金元宝需要实名认证后才能兑换。
+        src\\marketing-question\\questions  (1 usage found)
+            mainQuestion.js  (1 usage found)
+                53 <p>东方证券发行的<span class="text-red">金鳍-睿优</span>浮动收益率收益凭证，我可以在东方赢家财富版APP哪个菜单购买？</p>
+        src\\marketing-shareinstall  (1 usage found)
+            index.html  (1 usage found)
+                8 <title>东方赢家财富版</title>
+        src\\marketing-sharereverserepos\\views  (1 usage found)
+            template.js  (1 usage found)
+                46 下载东方赢家财富版APP
+        src\\privacy  (15 usages found)
+            policy.html  (6 usages found)
+                52 本政策适用于东方证券运营的互联网平台（包括东方赢家财富版APP、网上营业厅、H5页面、“东方微证券”微信服务号、“东方赢家”微信订阅号、微信小程序等）为您提供的所有产品和服务。<br/>
+                181 （二）我们的数据安全能力：目前我司互联网系统，包括东方赢家财富版APP和网站等均采用SSL技术进行加密，SSL协议版本为TLS 1.2，对称密钥长度为256位，主要的互联网系统如财富版APP和网站等，通过国家信息系统安全等级保护三级和二级的测评。同时符合证监会的相关技术指引中的要求。<br /> 
+                192 您有权访问您的个人信息，法律法规规定的例外情况除外。您可以通过登录东方赢家财富版APP，在“我的”中，查阅您的身份信息、账户信息等。如果您无法通过APP访问个人信息，您可以联系客服热线99503。 <br /><br />
+                194 当您发现我们处理的关于您的个人信息有错误时，您有权要求我们做出更正。您可以通过东方赢家财富版APP、网上营业厅或营业部现场提出更正申请。您也可以联系客服热线99503寻求帮助。<br /><br />
+                210 1、注销东方赢家财富版APP注册账户的，可以联系客服热线95503办理；<br />
+                211 2、注销资金账户、证券账户的，可以通过东方赢家财富版APP，“我的”中“业务办理”中“在线销户”办理。<br /><br />
+            register.html  (9 usages found)
+                58 <p class="font-weight">您同意，如本协议发生变更，东方证券将提前七个自然日通过东方赢家财富版APP或官方网站（www.dfzq.com.cn）予以公告，无需另行单独通知您。变更后的协议自公告期届满之日起生效。如您在协议生效后继续使用“东方赢家财富”服务，表示您接受并同意变更后的协议，也将遵循变更后的协议使用“东方赢家财富”服务。</p><br />
+                72 <p>（一）“东方赢家财富”服务：指东方证券通过东方赢家财富版APP客户端等互联网平台（如有新的平台，东方证券将以公告等形式发布）向用户提供的在线开户、在线交易、行情资讯、交易管理、客户服务等服务。</p>
+                110 <p class="font-weight">第十八条 当您使用“东方赢家财富”服务时，东方证券有权向您收取相关服务费用，收费方式以公司官方网站或东方赢家财富版APP上公示的为准，监管另有规定的除外。</p>
+                175 本政策适用于东方证券运营的互联网平台（包括东方赢家财富版APP、网上营业厅、H5页面、“东方微证券”微信服务号、“东方赢家”微信订阅号、微信小程序等）为您提供的所有产品和服务。<br/>
+                304 （二）我们的数据安全能力：目前我司互联网系统，包括东方赢家财富版APP和网站等均采用SSL技术进行加密，SSL协议版本为TLS 1.2，对称密钥长度为256位，主要的互联网系统如财富版APP和网站等，通过国家信息系统安全等级保护三级和二级的测评。同时符合证监会的相关技术指引中的要求。<br /> 
+                315 您有权访问您的个人信息，法律法规规定的例外情况除外。您可以通过登录东方赢家财富版APP，在“我的”中，查阅您的身份信息、账户信息等。如果您无法通过APP访问个人信息，您可以联系客服热线99503。 <br /><br />
+                317 当您发现我们处理的关于您的个人信息有错误时，您有权要求我们做出更正。您可以通过东方赢家财富版APP、网上营业厅或营业部现场提出更正申请。您也可以联系客服热线99503寻求帮助。<br /><br />
+                333 1、注销东方赢家财富版APP注册账户的，可以联系客服热线95503办理；<br />
+                334 2、注销资金账户、证券账户的，可以通过东方赢家财富版APP，“我的”中“业务办理”中“在线销户”办理。<br /><br />
+        src\\protocols  (4 usages found)
+            crjh-rules.html  (4 usages found)
+                42 <p>点击活动链接&ldquo;紫气东来&rdquo;，完成基础信息填写，下载安装东方赢家财富版APP并使用手机号码登录。</p>
+                161 <p>*本保单扩展承保东方赢家财富版APP中的个人账号，在该账号中的资金发生损失，保险人根据合同约定负责赔偿。该账号下任何投资股票的损失不在承保范围。</p>
+                187 <p>活动期间，客户完成活动要求，即可进入东方赢家财富版APP，点击【行情/自选】-【个股】，或点击【我的】-【我的Level2】即可获得的免费Level-2。</p>
+                191 <p>进入东方赢家财富版APP理财频道，点击【新手专享】根据产品要求购买产品。</p>
+    Usage in string constants  (17 usages found)
+        libs  (3 usages found)
+            dfzq-jsbridge-1.0.js.map  (1 usage found)
+                1 请在东方赢家财富版客户端打开链接
+            dfzq-jsbridge-1.1.js.map  (1 usage found)
+                1 请在东方赢家财富版客户端打开链接
+            dfzq-jsbridge-1.2.js.map  (1 usage found)
+                1 请在东方赢家财富版客户端打开链接
+        src\\common\\shareNavigation  (1 usage found)
+            index.js  (1 usage found)
+                95                         <span class="logoText">打开东方赢家财富版APP</span>
+        src\\home-informationdetail  (2 usages found)
+            index.html  (2 usages found)
+                68 <img src="https://h5itest1.dfzq.com.cn/html5-openlibs/news/v1.0.0/mask.jpg" alt="打开东方赢家财富版查看全文"
+                69 title="打开东方赢家财富版查看全文"/>
+        src\\ifeng-stockmatch\\actions  (1 usage found)
+            main.js  (1 usage found)
+                83 alertman('您已报名成功，赶紧下载东方赢家财富版APP参赛吧！',_ => actions.jsbridge.go({title:"",uri:"https://lnk0.com/easylink/ELI9480s"}))
+        src\\level2-activity  (1 usage found)
+            index.js  (1 usage found)
+                31 desc:'东方赢家财富版邀您体验股民标配Level2，畅享毫秒级极速行情！',
+        src\\marketing-celebration2  (2 usages found)
+            index.js  (2 usages found)
+                63 appShareTitle: '东方赢家财富版智能装备全新升级',
+                67 wxShareTitle: '东方赢家财富版智能装备全新升级',
+        src\\marketing-crjh2  (1 usage found)
+            index.js  (1 usage found)
+                25 desc: '东方赢家财富版APP用户专享',
+        src\\marketing-crjh2\\actions  (1 usage found)
+            main.js  (1 usage found)
+                113 alertman('提交成功<br>请前往下载东方赢家财富版app', _ => {
+        src\\marketing-fundFT  (1 usage found)
+            index.js  (1 usage found)
+                33 desc:'体验东方赢家财富版APP定投功能，发现更多惊喜！',
+        src\\marketing-install\\config  (1 usage found)
+            download.js  (1 usage found)
+                3 "title": "东方赢家财富版",
+        src\\marketing-kcb  (1 usage found)
+            index.js  (1 usage found)
+                24 desc: '东方赢家财富版APP，7*24小时，无需预约开通科创板！',
+        src\\marketing-oefundintro\\actions  (1 usage found)
+            events.js  (1 usage found)
+                62 alertman('请下载最新的东方赢家财富版APP，当前功能仅在APP中开放购买', _ => location.href = configuration.appDownloadUrl)
+        src\\marketing-question  (1 usage found)
+            index.js  (1 usage found)
+                66 info:"东方证券发行的金鳍-睿优浮动收益率收益凭证，我可以在东方赢家财富版APP理财频道购买。"
+Usages in generated code  (4 usages found)
+    Usage in string constants  (4 usages found)
+        libs  (4 usages found)
+            dfzq-jsbridge-1.0.js  (1 usage found)
+                10 请在东方赢家财富版客户端打开链接
+            dfzq-jsbridge-1.1.js  (1 usage found)
+                10 请在东方赢家财富版客户端打开链接
+            dfzq-jsbridge-1.2.js  (1 usage found)
+                10 请在东方赢家财富版客户端打开链接
+            dfzq-jsbridge-1.3.js  (1 usage found)
+                10 请在东方赢家财富版客户端打开链接
+
+
+`;
+
+const reg = /src\\.*\((\d+)\susage[s]?\sfound\)/g;
+const reg2 = /src\\(!?(.*)\\).*\((\d+)\susage[s]?\sfound\)/g;
+
+
+const arr = {};
+let tmp;
+while ((tmp = reg.exec(txt)) !== null) {
+    console.log(tmp[0].split(/\s\s/)[0].split("\\")[1],tmp[1]);
+
+    // expected output: "Found foo. Next starts at 9."
+    // expected output: "Found foo. Next starts at 19."
+}
+
+
